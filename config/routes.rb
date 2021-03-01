fd5a3829e6457eda11b569965ac89120b9d09d53
Alexandre Rojon <alexandre.rojon@gmail.com>
2021-03-01 18:23:23 +0100

    small changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'recipes/show'
-  get 'user_queries/new'
-  get 'user_queries/show'
-  get 'user_queries/index'
   devise_for :users
   root to: 'user_queries#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
