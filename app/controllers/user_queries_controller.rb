1846dd54bfc793f4a329e54c971b2bc0eb611e27
Sebastien DathDelcambe <sebastien.dathdelcambe@gmail.com>
2021-03-03 10:47:43 +0100

    fixing technical problem

--- app/controllers/user_queries_controller.rb
+++ app/controllers/user_queries_controller.rb
@@ -1,48 +1,48 @@
 class UserQueriesController < ApplicationController
 
-  def new
-  end
-
-  def create
-    response = RestClient.get 'https://api.spoonacular.com/recipes/complexSearch',
-      {
-        params: {"apiKey" => ENV["API_KEY_SPOON"], addRecipeInformation: true,
-         includeIngredients: params.dig(:query, :ingredients), number: 10, diet: params.dig(:query, :diet)}
-      }
-    recipes = JSON.parse(response.body)
-
- 
-  end
-
-
-
-
-
-    @user_query = UserQuery.new
-    @user_query.user = current_user
-    @user_query.name = "query name"
-    @user_query.save
-    recipes["results"].each do |recipe_hash|
-      recipe = Recipe.create!(
-        name: recipe_hash["title"],
-        ingredients: recipe_hash,
-        instructions: recipe_hash["analyzedInstructions"],
-        api_id: recipe_hash["id"]
-        )
-      @user_query.recipes << recipe
+    def new
     end
-
-    redirect_to user_query_path(@user_query)
-
+  
+    def create
+      response = RestClient.get 'https://api.spoonacular.com/recipes/complexSearch',
+        {
+          params: {"apiKey" => ENV["API_KEY_SPOON"], addRecipeInformation: true,
+           includeIngredients: params.dig(:query, :ingredients), number: 10, diet: params.dig(:query, :diet)}
+        }
+      recipes = JSON.parse(response.body)
+  
+   
+    end
+  
+  
+  
+  
+  
+      @user_query = UserQuery.new
+      @user_query.user = current_user
+      @user_query.name = "query name"
+      @user_query.save
+      recipes["results"].each do |recipe_hash|
+        recipe = Recipe.create!(
+          name: recipe_hash["title"],
+          ingredients: recipe_hash,
+          instructions: recipe_hash["analyzedInstructions"],
+          api_id: recipe_hash["id"]
+          )
+        @user_query.recipes << recipe
+      end
+  
+      redirect_to user_query_path(@user_query)
+  
+   
+  
+    def show
+        @user_query = UserQuery.find(params[:id])
+    end
+  
+    def index
+    end
+  
+  
   end
-
-  def show
-      @user_query = UserQuery.find(params[:id])
-  end
-
-  def index
-  end
-
-
-end
 
