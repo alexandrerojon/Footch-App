03f3a32bc978f1466c7a34ad6b6898717698bea7
arthur bary <arthurbary@gmail.com>
2021-03-03 11:56:52 +0100

    all meth done, link card ?, delete button?

--- app/models/user_query.rb
+++ app/models/user_query.rb
@@ -2,14 +2,5 @@ class UserQuery < ApplicationRecord
   belongs_to :user
   has_many :query_recipes
   has_many :recipes, through: :query_recipes
-  
-  def image
-    self.ingredients["image"]
-    
-  end
 
-  def subject
-    name = self.ingredients["title"]
-    return "#{name}"
-  end 
 end
