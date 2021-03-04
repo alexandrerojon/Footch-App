92bca08d889f743ef7dfa88041c174cf73fbf875
Allison Sessé <allisonsesse@gmail.com>
2021-03-04 11:50:22 +0100

    users adding ingredients

--- app/controllers/party_ingredients_controller.rb
+++ app/controllers/party_ingredients_controller.rb
@@ -1,9 +1,17 @@
 class PartyIngredientsController < ApplicationController
 
   def create
-    @party_ingredients = PartyIngredients.new
-    @party_ingredients.party_id = params[:party_id]
-    @party_ingredients.user = current.user
-    @party_ingredients.save
+    @user_party = UserParty.find(params[:user_party_id])
+    @party_ingredients = PartyIngredient.new(party_ingredients_params)
+    @party_ingredients.user_party = @user_party
+    if @party_ingredients.save
+      redirect_to party_path(@user_party.party)
+    end
+  end
+
+  private
+
+  def party_ingredients_params
+    params.require(:party_ingredient).permit(:name)
   end
 end
