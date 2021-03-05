55fe76174a889f195733a7eaf277840d028ab86f
Sebastien DathDelcambe <sebastien.dathdelcambe@gmail.com>
2021-03-05 15:23:33 +0100

    picture can be added to a recipe you just created

--- app/mailers/usermailer_mailer.rb
+++ app/mailers/usermailer_mailer.rb
@@ -1,15 +1,9 @@
 class UsermailerMailer < ApplicationMailer
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  #
-  ##{current_user.username}
-  #   @user = params[:user]
-  # perhaps put user as a parameter of the method and put  inside it in place of lane 9
   def invitation
-    @user = params[:user]
-    @party = params[:party_id]
     
-
-    mail(to: @user.email, subject: "You were invited in the party of {@party.user.username} !")
+    @user = User.find_by(params[:user_id])
+    @party = Party.find_by(params[:id])
+   
+      mail(to: @user.email, subject: "You were invited in the party of #{@user.username} !")
   end
 end
