8e239079ec0ff7075592659729daffcb5d15bc14
Sebastien DathDelcambe <sebastien.dathdelcambe@gmail.com>
2021-03-08 18:54:32 +0100

    voila

--- app/models/party.rb
+++ app/models/party.rb
@@ -2,7 +2,7 @@ class Party < ApplicationRecord
   belongs_to :user
   has_many :user_parties, dependent: :destroy
   has_many :party_ingredients, through: :user_parties, dependent: :destroy
-  has_many :party_recipes
+  has_many :party_recipes, dependent: :destroy
   has_many :recipes, through: :party_recipes
   has_many :users, through: :user_parties
   has_many :messages, dependent: :destroy
