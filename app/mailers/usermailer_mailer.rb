8e0d431b4069816d27bcaaba17a68284328c5cd0
Sebastien DathDelcambe <sebastien.dathdelcambe@gmail.com>
2021-03-08 11:59:05 +0100

    homepage added

--- app/mailers/usermailer_mailer.rb
+++ app/mailers/usermailer_mailer.rb
@@ -1,8 +1,8 @@
 class UsermailerMailer < ApplicationMailer
   def invitation
     
-    @user = User.find_by(params[:user_id])
-    @party = Party.find_by(params[:id])
+    @user = User.find_by(params[:id])
+    @party = Party.find_by(params[:party_id])
    
       mail(to: @user.email, subject: "You were invited in the party of #{@user.username} !")
   end
